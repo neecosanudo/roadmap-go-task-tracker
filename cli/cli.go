748ab07f549287bd716efe0d793c6a58b9a1a21e
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2025-02-20 18:03:15 -0300

    ✨feat: enhance CLI reader with update, delete, and args validation

--- cli/cli.go
+++ cli/cli.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func Reader(userTaskList *UserTaskList) string {
@@ -9,9 +11,37 @@ func Reader(userTaskList *UserTaskList) string {
 
 	switch args[0] {
 	case "add":
-		answer := userTaskList.CreateNewTask(args[1])
-		return answer
+		if len(args) != 2 {
+			return "invalid number of arguments"
+		}
+		return userTaskList.CreateNewTask(args[1])
+	case "list":
+		if len(args) == 1 {
+			return userTaskList.GetAllTasks()
+		}
+		return "work in progress"
+	case "delete":
+		if len(args) != 2 {
+			return "invalid number of arguments"
+		}
+		id := assertIdAsUint(args[1])
+		return userTaskList.DeleteTaskById(id)
+	case "update":
+		if len(args) != 3 {
+			return "invalid number of arguments"
+		}
+		id := assertIdAsUint(args[1])
+		return userTaskList.UpdateTaskById(id, args[2])
 	default:
-		return "command no valid"
+		return "command not valid"
 	}
+}
+
+func assertIdAsUint(idString string) uint {
+	id, err := strconv.ParseUint(idString, 10, 64)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return uint(id)
 }
